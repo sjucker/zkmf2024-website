606cf07be3da1683056e281fc40dabd74a4871a4
Stefan Jucker <stefan.jucker@gmail.com>
2023-12-04 14:17:45 +0100

    Add location images and 360-view

--- pages/lokal/[id].vue
+++ pages/lokal/[id].vue
@@ -5,6 +5,13 @@
     <div v-if="!pending && !error">
       <template v-if="data">
         <h2 class="text-blau">{{ data.name }}</h2>
+        <NuxtImg
+          v-if="data.cloudflareId"
+          provider="cloudflare"
+          :src="getImgSrc(data.cloudflareId)"
+          loading="lazy"
+          class="max-w-full lg:w-1/2 lg:ml-8"
+        ></NuxtImg>
         <ul>
           <li>
             <a :href="data.googleMapsAddress" target="_blank">{{ data.address }}</a>
@@ -24,6 +31,19 @@
             <a :href="data.juryfeedback.googleMapsAddress" target="_blank">{{ data.juryfeedback.name }}</a>
           </li>
         </ul>
+
+        <div v-if="data.kuulaId" class="clear-both">
+          <h2>360-Grad-Ansicht</h2>
+          <iframe
+            width="100%"
+            height="640"
+            frameborder="0"
+            allow="xr-spatial-tracking; gyroscope; accelerometer"
+            allowfullscreen
+            scrolling="no"
+            :src="getKuulaSrc(data.kuulaId)"
+          ></iframe>
+        </div>
       </template>
     </div>
   </div>
@@ -43,4 +63,12 @@ const { data, pending, error } = await useFetch<LocationDTO>(`${apiBase}/public/
 useHead({
   title: data?.value?.name ?? 'Lokal',
 })
+
+function getImgSrc(id: string): string {
+  return `${id}/public`
+}
+
+function getKuulaSrc(id: string): string {
+  return `https://kuula.co/share/${id}?logo=1&info=1&fs=0&vr=0&zoom=1&sd=1&thumbs=1&inst=de`
+}
 </script>
