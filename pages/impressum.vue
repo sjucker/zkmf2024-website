124471548d12c29ece025081e178e7acfd644667
Stefan Jucker <stefan.jucker@gmail.com>
2023-12-07 21:59:06 +0100

    Fix consent-overlay

--- pages/impressum.vue
+++ pages/impressum.vue
@@ -3,5 +3,13 @@
 <template>
   <div class="prose prose-img:rounded-lg lg:max-w-none lg:prose-img:float-right prose-a:text-blau">
     <h2 class="text-blau">Impressum</h2>
+    <p>
+      <strong>Verein "zkmf2024"</strong><br />
+      Urs Dannenmann<br />
+      Im Grüt 52<br />
+      8902 Urdorf
+    </p>
+    <p>info@zkmf2024.ch</p>
+    <h3>Datenschutzerklärung</h3>
   </div>
 </template>
