f853aae5f3217f411a9879e1d0201431a4c9f1df
Stefan Jucker <stefan.jucker@gmail.com>
2023-12-09 10:22:00 +0100

    Impressum und Datenschutz

--- components/Consent.vue
+++ components/Consent.vue
@@ -1,17 +1,16 @@
 <template>
-  <div
-    v-if="needsConsentDecision"
-    class="min-w-full fixed bottom-0 left-0 bg-blau p-4 z-50 border-t-4 border-t-rot flex flex-col md:flex-row gap-4 text-xs justify-between"
-  >
-    <div class="text-white">
-      Wenn Sie auf «Akzeptieren» klicken, stimmen Sie der Speicherung von Cookies auf Ihrem Gerät zu, um die Nutzung der Website zu analysieren und unsere
-      Marketingaktivitäten zu unterstützen.<br />
-      Weitere Informationen finden Sie in unserer
-      <NuxtLink to="/datenschutzrichtlinie" class="underline">Datenschutzrichtlinie</NuxtLink>
-    </div>
-    <div class="flex flex-col-reverse md:flex-row gap-4 text-sm">
-      <button class="bg-transparent border-2 border-white text-white py-3 px-8 rounded" @click="revoke()">Ablehnen</button>
-      <button class="bg-white text-blau py-3 px-8 rounded" @click="accept()">Akzeptieren</button>
+  <div v-if="needsConsentDecision" class="min-w-full fixed bottom-0 left-0 bg-blau p-4 z-50 border-t-4 border-t-rot">
+    <div class="container mx-auto flex flex-col md:flex-row gap-4 text-xs justify-between">
+      <div class="text-white">
+        Wenn Sie auf «Akzeptieren» klicken, stimmen Sie der Speicherung von (nicht-essentiellen) Cookies auf Ihrem Gerät zu, um die Nutzung der Website zu
+        analysieren und die Benutzerfreundlichkeit unserer Website zu optimieren.<br />
+        Weitere Informationen finden Sie in unserer
+        <NuxtLink to="/datenschutz" class="underline">Datenschutzerklärung</NuxtLink>
+      </div>
+      <div class="flex flex-col-reverse md:flex-row gap-4 text-sm">
+        <button class="bg-transparent border-2 border-white text-white py-3 px-8 rounded" @click="revoke()">Ablehnen</button>
+        <button class="bg-white text-blau py-3 px-8 rounded" @click="accept()">Akzeptieren</button>
+      </div>
     </div>
   </div>
 </template>
