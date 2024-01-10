c1686693ecbbaaccc8d0bbcf12d2a467ed4f7eab
Stefan Jucker <jucker@mptechnology.ch>
2024-01-10 14:21:07 +0100

    Placeholder page for App

---
+++ pages/app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="prose prose-img:rounded-lg lg:max-w-none lg:prose-img:float-right prose-a:text-blau">
+    <h2 class="text-blau">Fest-App</h2>
+    <p>Die ZKMF2024-App wird Anfang Mai für Android und iOS veröffentlicht werden.</p>
+  </div>
+</template>
+<script setup lang="ts">
+useHead({
+  title: 'App',
+  meta: [
+    {
+      name: 'description',
+      content: 'Die App für das ZKMF2024 für Android und iOS.',
+    },
+  ],
+})
+</script>
