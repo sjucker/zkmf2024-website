22ee2bd8b31668b188b39baca823dfe1fc48ffe4
Stefan Jucker <stefan.jucker@gmail.com>
2024-02-11 10:22:15 +0100

    Add GeoJSON source

--- pages/karte.vue
+++ pages/karte.vue
@@ -4,13 +4,14 @@
 
     <MapboxMap
       map-id="1"
-      style="position: relative; width: 500px; height: 500px"
+      style="position: relative; width: 500px; height: 500px; max-width: 100%"
       :options="{
         style: 'mapbox://styles/mapbox/standard',
         center: [8.424727317603, 47.386092494172],
         zoom: 12,
       }"
     >
+      <MapboxSource source-id="test" :source="source" />
       <MapboxGeolocateControl position="top-left" />
     </MapboxMap>
   </div>
@@ -18,5 +19,14 @@
 <script setup lang="ts">
 useHead({
   title: 'Karte Festareal',
+})
+
+const {
+  public: { apiBase },
+} = useRuntimeConfig()
+
+const source = ref({
+  type: 'geojson',
+  data: `${apiBase}/public/location/geojson`,
 })
 </script>
