a92e49d51ec6ebea702f84f7fbda5aa330ab55c8
Stefan Jucker <stefan.jucker@gmail.com>
2024-02-19 10:17:26 +0100

    Revert to static map

--- pages/karte.vue
+++ pages/karte.vue
@@ -1,32 +1,12 @@
 <template>
   <div class="prose prose-a:text-blau">
     <h2 class="text-blau">Karte Festareal</h2>
-
-    <MapboxMap
-      map-id="1"
-      style="position: relative; width: 500px; height: 500px; max-width: 100%"
-      :options="{
-        style: 'mapbox://styles/mapbox/standard',
-        center: [8.424727317603, 47.386092494172],
-        zoom: 12,
-      }"
-    >
-      <MapboxSource source-id="test" :source="source" />
-      <MapboxGeolocateControl position="top-left" />
-    </MapboxMap>
+    <nuxt-img src="/info/karte.jpg" quality="95" class="max-w-full rounded" />
+    Als PDF <a href="/info/karte.pdf" target="_blank">herunterladen</a>
   </div>
 </template>
 <script setup lang="ts">
 useHead({
   title: 'Karte Festareal',
-})
-
-const {
-  public: { apiBase },
-} = useRuntimeConfig()
-
-const source = ref({
-  type: 'geojson',
-  data: `${apiBase}/public/location/geojson`,
 })
 </script>
