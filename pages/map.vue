178ed9c498ebe0c3e2b09ab95e522e9d22e47394
Stefan Jucker <stefan.jucker@gmail.com>
2024-02-25 10:02:55 +0100

    Add all relevant layers

--- pages/map.vue
+++ pages/map.vue
@@ -1,12 +1,8 @@
 <template>
   <nav id="menu" class="bg-white absolute top-4 left-4 z-10 rounded border-2 border-gray-300 p-2 text-sm">
-    <div class="flex gap-2">
-      <input type="checkbox" @click="toggleLayer('locations-wettspiel')" checked id="wettspiel" />
-      <label for="wettspiel" class="bg-green-500 text-white flex-1 p-1">Wettspiel</label>
-    </div>
-    <div class="flex gap-2">
-      <input type="checkbox" @click="toggleLayer('locations-einspiel')" id="einspiel" />
-      <label for="einspiel" class="bg-red-400 text-white flex-1 p-1">Einspielen</label>
+    <div class="flex gap-2" v-for="layer in layers" :key="layer.type">
+      <input type="checkbox" @click="toggleLayer(layer.type)" :checked="layer.checked" :id="layer.type" />
+      <label :for="layer.type" class="text-white flex-1 p-1" :style="{ backgroundColor: layer.color }">{{ layer.name }}</label>
     </div>
   </nav>
   <div id="map" ref="mapContainer" class="relative h-dvh w-dvw"></div>
@@ -14,7 +10,53 @@
 <script setup lang="ts">
 import mapboxgl from 'mapbox-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
+import { LocationType } from '~/types/rest'
 
+export interface LayerType {
+  type: LocationType
+  name: string
+  checked: boolean
+  color: string
+}
+
+const layers: LayerType[] = [
+  {
+    type: LocationType.WETTSPIELLOKAL,
+    checked: true,
+    name: 'Wettspiellokal',
+    color: '#26b080',
+  },
+  {
+    type: LocationType.EINSPIELLOKAL,
+    checked: false,
+    name: 'Einspiellokal',
+    color: '#3e99d1',
+  },
+  {
+    type: LocationType.JURYFEEDBACK,
+    checked: false,
+    name: 'Jury Gespräch',
+    color: '#9d66a6',
+  },
+  {
+    type: LocationType.INSTRUMENTENDEPOT,
+    checked: false,
+    name: 'Instrumentendepot',
+    color: '#fec800',
+  },
+  {
+    type: LocationType.PLATZKONZERT,
+    checked: false,
+    name: 'Festzelt & Food Meile',
+    color: '#ec4f02',
+  },
+  {
+    type: LocationType.PARADEMUSIK,
+    checked: false,
+    name: 'Parademusik',
+    color: '#004e93',
+  },
+]
 definePageMeta({
   layout: 'empty',
 })
@@ -37,7 +79,7 @@ onMounted(() => {
     container: mapContainer.value,
     style: 'mapbox://styles/mapbox/light-v11',
     center: [8.434727317603, 47.388092494172],
-    zoom: 14,
+    zoom: 13,
   })
 
   map.on('load', () => {
@@ -46,155 +88,91 @@ onMounted(() => {
       data: `${apiBase}/public/location/geojson`,
     })
 
-    map.addLayer({
-      id: 'locations-wettspiel',
-      type: 'circle',
-      source: 'locations',
-      filter: ['==', ['get', 'type'], 'WETTSPIELLOKAL'],
-      layout: {
-        visibility: 'visible',
-      },
-      paint: {
-        'circle-color': [
-          'match',
-          ['get', 'type'],
-          'JURYFEEDBACK',
-          '#b471d0',
-          'WETTSPIELLOKAL',
-          '#33b71d',
-          'EINSPIELLOKAL',
-          '#e55e5e',
-          'INSTRUMENTENDEPOT',
-          '#3bb2d0',
-          /* other */ '#ccc',
-        ],
-        'circle-radius': {
-          stops: [
-            [14, 10],
-            [15, 20],
-            [16, 40],
-            [17, 60],
-            [18, 80],
-          ],
+    for (const layer of layers.toReversed()) {
+      map.addLayer({
+        id: layer.type,
+        type: 'circle',
+        source: 'locations',
+        filter: ['==', ['get', 'type'], layer.type],
+        layout: {
+          visibility: layer.checked ? 'visible' : 'none',
         },
-        'circle-stroke-width': 2,
-        'circle-stroke-color': '#FFF',
-      },
-    })
-
-    map.addLayer({
-      id: 'locations-wettspiel-names',
-      type: 'symbol',
-      source: 'locations',
-      filter: ['==', ['get', 'type'], 'WETTSPIELLOKAL'],
-      layout: {
-        'text-anchor': 'center',
-        'text-field': ['get', 'id'],
-        'text-size': {
-          stops: [
-            [14, 12],
-            [15, 20],
-            [16, 30],
-            [17, 40],
-            [18, 50],
-          ],
+        paint: {
+          'circle-color': layer.color,
+          'circle-radius': {
+            stops: [
+              [14, 10],
+              [15, 20],
+              [16, 40],
+              [17, 60],
+              [18, 80],
+            ],
+          },
+          'circle-stroke-width': 2,
+          'circle-stroke-color': '#FFF',
         },
-      },
-      paint: {
-        'text-color': 'white',
-      },
-    })
-
-    map.addLayer({
-      id: 'locations-einspiel',
-      type: 'circle',
-      source: 'locations',
-      filter: ['==', ['get', 'type'], 'EINSPIELLOKAL'],
-      layout: {
-        visibility: 'none',
-      },
-      paint: {
-        'circle-color': [
-          'match',
-          ['get', 'type'],
-          'JURYFEEDBACK',
-          '#b471d0',
-          'WETTSPIELLOKAL',
-          '#33b71d',
-          'EINSPIELLOKAL',
-          '#e55e5e',
-          'INSTRUMENTENDEPOT',
-          '#3bb2d0',
-          /* other */ '#ccc',
-        ],
-        'circle-radius': {
-          stops: [
-            [14, 10],
-            [15, 20],
-            [16, 40],
-            [17, 60],
-            [18, 80],
-          ],
-        },
-        'circle-stroke-width': 2,
-        'circle-stroke-color': '#FFF',
-      },
-    })
-
-    map.addLayer({
-      id: 'locations-einspiel-names',
-      type: 'symbol',
-      source: 'locations',
-      filter: ['==', ['get', 'type'], 'EINSPIELLOKAL'],
-      layout: {
-        visibility: 'none',
-        'text-anchor': 'center',
-        'text-field': ['get', 'id'],
-        'text-size': {
-          stops: [
-            [14, 12],
-            [15, 20],
-            [16, 30],
-            [17, 40],
-            [18, 50],
-          ],
-        },
-      },
-      paint: {
-        'text-color': 'white',
-      },
-    })
-
-    map.on('click', 'locations-wettspiel', e => {
-      const features = map.queryRenderedFeatures(e.point, {
-        layers: ['locations-wettspiel'],
       })
 
-      const coordinates = features[0].geometry.coordinates.slice()
-
-      // Ensure that if the map is zoomed out such that
-      // multiple copies of the feature are visible, the
-      // popup appears over the copy being pointed to.
-      while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360
-      }
-
-      new mapboxgl.Popup({
-        closeButton: false,
-        closeOnMove: true,
+      map.addLayer({
+        id: `${layer.type}-names`,
+        type: 'symbol',
+        source: 'locations',
+        filter: ['==', ['get', 'type'], layer.type],
+        layout: {
+          visibility: layer.checked ? 'visible' : 'none',
+          'text-anchor': 'center',
+          'text-field': ['get', 'id'],
+          'text-size': {
+            stops: [
+              [14, 12],
+              [15, 20],
+              [16, 30],
+              [17, 40],
+              [18, 50],
+            ],
+          },
+        },
+        paint: {
+          'text-color': 'white',
+        },
       })
-        .setLngLat(coordinates)
-        .setHTML(`<strong>${features[0]?.properties?.name}</strong><br/>Module: ${features[0]?.properties?.info}`)
-        .addTo(map)
-    })
 
-    map.on('mouseenter', 'locations-wettspiel', e => {
-      map.getCanvas().style.cursor = 'pointer'
-    })
+      map.on('click', layer.type, e => {
+        const features = map.queryRenderedFeatures(e.point, {
+          layers: [layer.type],
+        })
 
-    map.on('mouseleave', 'locations-wettspiel', () => {
-      map.getCanvas().style.cursor = 'default'
-    })
+        const coordinates = features[0].geometry.coordinates.slice()
+
+        // Ensure that if the map is zoomed out such that
+        // multiple copies of the feature are visible, the
+        // popup appears over the copy being pointed to.
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+          coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360
+        }
+
+        new mapboxgl.Popup({
+          closeButton: false,
+          closeOnMove: true,
+        })
+          .setLngLat(coordinates)
+          .setHTML(`<strong>${features[0]?.properties?.name}</strong><br/>${features[0]?.properties?.info}`)
+          .addTo(map)
+      })
+
+      map.on('mouseenter', layer.type, e => {
+        map.getCanvas().style.cursor = 'pointer'
+      })
+
+      map.on('mouseleave', layer.type, () => {
+        map.getCanvas().style.cursor = 'default'
+      })
+    }
+
+    map.fitBounds([
+      [8.42018651050729, 47.37787047581315], // southwestern corner of the bounds
+      [8.45040397121158, 47.400236075092586], // northeastern corner of the bounds
+    ])
   })
 
   map.addControl(
