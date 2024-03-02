e0f6271837b82507091663d7e69c5f73208f6c7d
Stefan Jucker <stefan.jucker@gmail.com>
2024-03-02 10:26:34 +0100

    Update Karte Festareal

--- pages/karte.vue
+++ pages/karte.vue
@@ -1,11 +1,20 @@
 <template>
-  <div class="prose prose-a:text-blau">
+  <div class="prose prose-a:text-blau max-w-none">
     <h2 class="text-blau">Karte Festareal</h2>
-    <nuxt-img src="/info/karte.jpg" quality="95" class="max-w-full rounded" />
+    <NuxtLink to="/map" class="inline-flex gap-1">
+      <MapPinIcon class="h-6 w-6 text-blau" />
+      Interaktive Karte
+    </NuxtLink>
+    <div class="flex flex-col xl:flex-row gap-2">
+      <nuxt-img src="/info/karte-schlieren.jpg" quality="95" class="max-w-full w-[600px] rounded" />
+      <nuxt-img src="/info/karte-urdorf.jpg" quality="95" class="max-w-full w-[600px] rounded" />
+    </div>
     Als PDF <a href="/info/karte.pdf" target="_blank">herunterladen</a>
   </div>
 </template>
 <script setup lang="ts">
+import { MapPinIcon } from '@heroicons/vue/24/outline'
+
 useSeoMeta({
   title: 'Karte Festareal',
 })
