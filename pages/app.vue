975089022d95e463fc6db80f787b5698a0dcf7c0
Stefan Jucker <stefan.jucker@gmail.com>
2024-03-14 22:17:10 +0100

    App landing page

--- pages/app.vue
+++ pages/app.vue
@@ -1,7 +1,17 @@
 <template>
-  <div class="prose prose-img:rounded-lg lg:max-w-none lg:prose-img:float-right prose-a:text-blau">
-    <h2 class="text-blau">Fest-App</h2>
-    <p>Die ZKMF2024-App wird Anfang Mai für Android und iOS veröffentlicht werden.</p>
+  <div class="prose">
+    <h2 class="text-blau">Festprogramm-App</h2>
+    <h3 class="text-rot">Die ZKMF2024-App kommt Ende Mai!</h3>
+    <p>Die neusten Informationen, alle Spielzeiten auf einen Blick, eine interaktive Karte, Benachrichtigungen für deine Lieblingsvereine und vieles mehr!</p>
+  </div>
+  <div class="flex flex-col sm:flex-row gap-2 sm:gap-5 pt-5">
+    <NuxtImg src="/app/google-play.png" quality="90" class="max-w-full opacity-20"></NuxtImg>
+    <NuxtImg src="/app/app-store.png" quality="90" class="max-w-full opacity-20"></NuxtImg>
+  </div>
+  <div class="container flex flex-col sm:flex-row gap-10 pt-5">
+    <NuxtImg src="/app/screen1.png" quality="90" class="w-full sm:w-1/3 lg:w-1/5 max-w-full"></NuxtImg>
+    <NuxtImg src="/app/screen3.png" quality="90" class="w-full sm:w-1/3 lg:w-1/5 max-w-full"></NuxtImg>
+    <NuxtImg src="/app/screen2.png" quality="90" class="w-full sm:w-1/3 lg:w-1/5 max-w-full"></NuxtImg>
   </div>
 </template>
 <script setup lang="ts">
