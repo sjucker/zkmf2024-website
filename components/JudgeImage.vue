9ad3858615c5d32f71cb9a6f62e1c73ca0a241c1
Stefan Jucker <stefan.jucker@gmail.com>
2024-04-02 21:31:05 +0200

    Add modul per judge

--- components/JudgeImage.vue
+++ components/JudgeImage.vue
@@ -1,12 +1,22 @@
 <template>
-  <div class="flex flex-col items-center flex-wrap max-w-full md:w-[300px] p-3 md:p-6">
-    <NuxtImg provider="cloudflare" :src="cloudflareUrl(src)" loading="lazy" :alt="name" :title="name" class="rounded-full shadow shadow-gray-500" />
-    <div>{{ name }}</div>
+  <div class="flex flex-col items-center flex-wrap max-w-full sm:w-[300px] lg:w-[350px] p-3 md:p-6">
+    <NuxtImg
+      provider="cloudflare"
+      :src="cloudflareUrl(src)"
+      loading="lazy"
+      :alt="name"
+      :title="name"
+      class="rounded-full shadow shadow-gray-500"
+      densities="x1"
+    />
+    <div class="font-bold text-center">{{ name }}</div>
+    <div class="text-sm text-center whitespace-pre">{{ modul }}</div>
   </div>
 </template>
 <script setup lang="ts">
 defineProps<{
   src: string
   name: string
+  modul: string
 }>()
 </script>
