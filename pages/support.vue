89e68024aa9ada34f9981fcd48abc25729a5db53
Stefan Jucker <jucker@mptechnology.ch>
2024-05-28 13:02:47 +0200

    Fix typo

--- pages/support.vue
+++ pages/support.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="prose">
-    <h2 class="text-rot">Suppor Festprogramm-App</h2>
+    <h2 class="text-rot">Support Festprogramm-App</h2>
     <p>Haben Sie Fragen oder Feedback zu unserer App, dann melden Sie sich <a href="mailto:support@zkmf2024.ch">hier</a>.</p>
   </div>
 </template>
