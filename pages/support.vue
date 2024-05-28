79f5b181890e9703058b1e0fed684cd883f0a3ce
Stefan Jucker <jucker@mptechnology.ch>
2024-05-28 13:01:40 +0200

    Add app support email

--- pages/support.vue
+++ pages/support.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="prose">
     <h2 class="text-rot">Suppor Festprogramm-App</h2>
-    <p>Haben Sie Fragen oder Feedback zu unserer App, dann melden Sie sich <a href="mailto:info@zkmf2024.ch">hier</a>.</p>
+    <p>Haben Sie Fragen oder Feedback zu unserer App, dann melden Sie sich <a href="mailto:support@zkmf2024.ch">hier</a>.</p>
   </div>
 </template>
 <script setup lang="ts"></script>
