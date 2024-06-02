92707779e8ac8552af99826dee0605c9b44caf36
Stefan Jucker <stefan.jucker@gmail.com>
2024-06-02 18:00:31 +0200

    New Tätigkeitsbeschrieb - Wettspiel JurysekretärIn_v1.1

--- pages/taetigkeitsbeschriebe.vue
+++ pages/taetigkeitsbeschriebe.vue
@@ -3,65 +3,85 @@
     <h2 class="text-rot">Tätigkeitsbeschriebe</h2>
     <h3>Bereich Verpflegung</h3>
     <ul>
-      <li><a href="/helfer/Tätigkeitsbeschrieb - Verpflegung Küche LeiterIn_v1.0.pdf" target="_blank">Küche - Leitung</a></li>
-      <li><a href="/helfer/Tätigkeitsbeschrieb - Verpflegung Küche Anrichten_v1.0.pdf" target="_blank">Küche - Anrichten</a></li>
-      <li><a href="/helfer/Tätigkeitsbeschrieb - Verpflegung Küche Grill und Fritteuse_v1.0.pdf" target="_blank">Küche - Grill und Fritteuse</a></li>
-      <li><a href="/helfer/Tätigkeitsbeschrieb - Verpflegung Kasse und Warenausgabe_v1.0.pdf" target="_blank">Kasse und Warenausgabe</a></li>
-      <li><a href="/helfer/Tätigkeitsbeschrieb - Verpflegung Service LeiterIn_v1.0.pdf" target="_blank">Service - Leitung</a></li>
-      <li><a href="/helfer/Tätigkeitsbeschrieb - Verpflegung Getränke Essen bringen_v1.0.pdf" target="_blank">Service - Getränke / Essen bringen</a></li>
-      <li><a href="/helfer/Tätigkeitsbeschrieb - Verpflegung Service Bestellung Inkasso_v1.0.pdf" target="_blank">Service - Bestellung und Inkasso</a></li>
-      <li><a href="/helfer/Tätigkeitsbeschrieb - Verpflegung Service Abräumen_v1.0.pdf" target="_blank">Service - Abräumen</a></li>
-      <li><a href="/helfer/Tätigkeitsbeschrieb - Verpflegung SpringerIn_v1.0.pdf" target="_blank">Springer / Springerin Verpflegung</a></li>
-      <li><a href="/helfer/Tätigkeitsbeschrieb - Verpflegung Service Bar_v1.0.pdf" target="_blank">Service - Bar</a></li>
-      <li><a href="/helfer/Tätigkeitsbeschrieb - Verpflegung Nach-Rückschub Entsorgung_v1.0.pdf" target="_blank">Nachschub / Rückschub / Entsorgung</a></li>
-      <li><a href="/helfer/Tätigkeitsbeschrieb - Verpflegung Getränkestand Betrieb_v1.0.pdf" target="_blank">Betrieb Getränkestand</a></li>
-      <li><a href="/helfer/Tätigkeitsbeschrieb - Verpflegung VIP-Apero_v1.0.pdf" target="_blank">VIP-Apero</a></li>
+      <li><a href="/helfer/Tätigkeitsbeschrieb%20-%20Verpflegung%20Küche%20LeiterIn_v1.0.pdf" target="_blank">Küche - Leitung</a></li>
+      <li><a href="/helfer/Tätigkeitsbeschrieb%20-%20Verpflegung%20Küche%20Anrichten_v1.0.pdf" target="_blank">Küche - Anrichten</a></li>
+      <li>
+        <a href="/helfer/Tätigkeitsbeschrieb%20-%20Verpflegung%20Küche%20Grill%20und%20Fritteuse_v1.0.pdf" target="_blank">Küche - Grill und Fritteuse</a>
+      </li>
+      <li><a href="/helfer/Tätigkeitsbeschrieb%20-%20Verpflegung%20Kasse%20und%20Warenausgabe_v1.0.pdf" target="_blank">Kasse und Warenausgabe</a></li>
+      <li><a href="/helfer/Tätigkeitsbeschrieb%20-%20Verpflegung%20Service%20LeiterIn_v1.0.pdf" target="_blank">Service - Leitung</a></li>
+      <li>
+        <a href="/helfer/Tätigkeitsbeschrieb%20-%20Verpflegung%20Getränke%20Essen%20bringen_v1.0.pdf" target="_blank">Service - Getränke / Essen bringen</a>
+      </li>
+      <li>
+        <a href="/helfer/Tätigkeitsbeschrieb%20-%20Verpflegung%20Service%20Bestellung%20Inkasso_v1.0.pdf" target="_blank">Service - Bestellung und Inkasso</a>
+      </li>
+      <li><a href="/helfer/Tätigkeitsbeschrieb%20-%20Verpflegung%20Service%20Abräumen_v1.0.pdf" target="_blank">Service - Abräumen</a></li>
+      <li><a href="/helfer/Tätigkeitsbeschrieb%20-%20Verpflegung%20SpringerIn_v1.0.pdf" target="_blank">Springer / Springerin Verpflegung</a></li>
+      <li><a href="/helfer/Tätigkeitsbeschrieb%20-%20Verpflegung%20Service%20Bar_v1.0.pdf" target="_blank">Service - Bar</a></li>
+      <li>
+        <a href="/helfer/Tätigkeitsbeschrieb%20-%20Verpflegung%20Nach-Rückschub%20Entsorgung_v1.0.pdf" target="_blank">Nachschub / Rückschub / Entsorgung</a>
+      </li>
+      <li><a href="/helfer/Tätigkeitsbeschrieb%20-%20Verpflegung%20Getränkestand%20Betrieb_v1.0.pdf" target="_blank">Betrieb Getränkestand</a></li>
+      <li><a href="/helfer/Tätigkeitsbeschrieb%20-%20Verpflegung%20VIP-Apero_v1.0.pdf" target="_blank">VIP-Apero</a></li>
     </ul>
     <hr class="my-10" />
     <h3>Bereich Wettspiel</h3>
     <ul>
-      <li><a href="/helfer/Tätigkeitsbeschrieb - Wettspiel JurysekretärIn_v1.0.pdf" target="_blank">Jurysekretär / Jurysekretärin</a></li>
-      <li><a href="/helfer/Tätigkeitsbeschrieb - Wettspiel JurybetreuerIn mobil_v1.0.pdf" target="_blank">Jurybetreuung mobil</a></li>
-      <li><a href="/helfer/Tätigkeitsbeschrieb - Wettspiel Wettspiellokal LokalchefIn_v1.0.pdf" target="_blank">Chef / Chefin Wettspiellokal</a></li>
-      <li><a href="/helfer/Tätigkeitsbeschrieb - Wettspiel Moderation_v1.1.pdf" target="_blank">Moderation</a></li>
-      <li><a href="/helfer/Tätigkeitsbeschrieb - Wettspiel Wettspiellokal Einlasskontrolle_v1.0.pdf" target="_blank">Einlasskontrolle Wettspiellokal</a></li>
-      <li><a href="/helfer/Tätigkeitsbeschrieb - Wettspiel Wettspiel Roadie_v1.0.pdf" target="_blank">Roadie Wettspiel</a></li>
-      <li><a href="/helfer/Tätigkeitsbeschrieb - Wettspiel Marschmusik-HelferIn_v1.0.pdf" target="_blank">Marschmusik Helfer / Helferin</a></li>
-      <li><a href="/helfer/Tätigkeitsbeschrieb - Wettspiel Marschmusik TafelträgerIn_v1.0.pdf" target="_blank">Marschmusik Tafelträger / Tafelträgerin</a></li>
+      <li><a href="/helfer/Tätigkeitsbeschrieb%20-%20Wettspiel%20JurysekretärIn_v1.1.pdf" target="_blank">Jurysekretär / Jurysekretärin</a></li>
+      <li><a href="/helfer/Tätigkeitsbeschrieb%20-%20Wettspiel%20JurybetreuerIn%20mobil_v1.0.pdf" target="_blank">Jurybetreuung mobil</a></li>
+      <li><a href="/helfer/Tätigkeitsbeschrieb%20-%20Wettspiel%20Wettspiellokal%20LokalchefIn_v1.0.pdf" target="_blank">Chef / Chefin Wettspiellokal</a></li>
+      <li><a href="/helfer/Tätigkeitsbeschrieb%20-%20Wettspiel%20Moderation_v1.1.pdf" target="_blank">Moderation</a></li>
       <li>
-        <a href="/helfer/Tätigkeitsbeschrieb - Wettspiel Wettspiellokal BühnenchefIn_v1.0.pdf" target="_blank">Bühnenchef / Bühnenchefin Wettspiellokal</a>
+        <a href="/helfer/Tätigkeitsbeschrieb%20-%20Wettspiel%20Wettspiellokal%20Einlasskontrolle_v1.0.pdf" target="_blank">Einlasskontrolle Wettspiellokal</a>
       </li>
-      <li><a href="/helfer/Tätigkeitsbeschrieb - Wettspiel Chef Wettspiel Platzkonzerte_v1.0.pdf" target="_blank">Chef / Chefin Wettspiel Platzkonzerte</a></li>
-      <li><a href="/helfer/Tätigkeitsbeschrieb - Wettspiel Einspiellokal Einlasskontrolle_v1.0.pdf" target="_blank">Einlasskontrolle Einspiellokal</a></li>
-      <li><a href="/helfer/Tätigkeitsbeschrieb - Wettspiel Einspiellokal LokalchefIn_v1.0.pdf" target="_blank">Chef / Chefin Einspiellokal</a></li>
+      <li><a href="/helfer/Tätigkeitsbeschrieb%20-%20Wettspiel%20Wettspiel%20Roadie_v1.0.pdf" target="_blank">Roadie Wettspiel</a></li>
+      <li><a href="/helfer/Tätigkeitsbeschrieb%20-%20Wettspiel%20Marschmusik-HelferIn_v1.0.pdf" target="_blank">Marschmusik Helfer / Helferin</a></li>
+      <li>
+        <a href="/helfer/Tätigkeitsbeschrieb%20-%20Wettspiel%20Marschmusik%20TafelträgerIn_v1.0.pdf" target="_blank">Marschmusik Tafelträger / Tafelträgerin</a>
+      </li>
+      <li>
+        <a href="/helfer/Tätigkeitsbeschrieb%20-%20Wettspiel%20Wettspiellokal%20BühnenchefIn_v1.0.pdf" target="_blank"
+          >Bühnenchef / Bühnenchefin Wettspiellokal</a
+        >
+      </li>
+      <li>
+        <a href="/helfer/Tätigkeitsbeschrieb%20-%20Wettspiel%20Chef%20Wettspiel%20Platzkonzerte_v1.0.pdf" target="_blank"
+          >Chef / Chefin Wettspiel Platzkonzerte</a
+        >
+      </li>
+      <li>
+        <a href="/helfer/Tätigkeitsbeschrieb%20-%20Wettspiel%20Einspiellokal%20Einlasskontrolle_v1.0.pdf" target="_blank">Einlasskontrolle Einspiellokal</a>
+      </li>
+      <li><a href="/helfer/Tätigkeitsbeschrieb%20-%20Wettspiel%20Einspiellokal%20LokalchefIn_v1.0.pdf" target="_blank">Chef / Chefin Einspiellokal</a></li>
     </ul>
     <hr class="my-10" />
     <h3>Bereich Rahmenprogramm</h3>
     <ul>
-      <li><a href="/helfer/Tätigkeitsbeschrieb - Rahmenprogramm Roadie Bühne_v1.0.pdf" target="_blank">Roadie Bühne</a></li>
-      <li><a href="/helfer/Tätigkeitsbeschrieb - Rahmenprogramm HelferIn Bandraum_v1.0.pdf" target="_blank">Helfer / Helferin Bandraum</a></li>
+      <li><a href="/helfer/Tätigkeitsbeschrieb%20-%20Rahmenprogramm%20Roadie%20Bühne_v1.0.pdf" target="_blank">Roadie Bühne</a></li>
+      <li><a href="/helfer/Tätigkeitsbeschrieb%20-%20Rahmenprogramm%20HelferIn%20Bandraum_v1.0.pdf" target="_blank">Helfer / Helferin Bandraum</a></li>
     </ul>
     <hr class="my-10" />
     <h3>Bereich Infrastruktur</h3>
     <ul>
       <li>
-        <a href="/helfer/Tätigkeitsbeschrieb - Infrastruktur Tische Festbänke Festzelt_v1.0.pdf" target="_blank"
+        <a href="/helfer/Tätigkeitsbeschrieb%20-%20Infrastruktur%20Tische%20Festbänke%20Festzelt_v1.0.pdf" target="_blank"
           >Aufbau / Rückbau Tische + Festbänke Festzelt</a
         >
       </li>
-      <li><a href="/helfer/Tätigkeitsbeschrieb - Infrastruktur Entfernen Stühle Festzelt_v1.0.pdf" target="_blank">Entfernen Stühle Festzelt</a></li>
-      <li><a href="/helfer/Tätigkeitsbeschrieb - Infrastruktur Entsorgung Festgelände_v1.0.pdf" target="_blank">Entsorgung Festgelände</a></li>
+      <li><a href="/helfer/Tätigkeitsbeschrieb%20-%20Infrastruktur%20Entfernen%20Stühle%20Festzelt_v1.0.pdf" target="_blank">Entfernen Stühle Festzelt</a></li>
+      <li><a href="/helfer/Tätigkeitsbeschrieb%20-%20Infrastruktur%20Entsorgung%20Festgelände_v1.0.pdf" target="_blank">Entsorgung Festgelände</a></li>
     </ul>
     <hr class="my-10" />
     <h3>Bereich Sekretariat</h3>
     <ul>
-      <li><a href="/helfer/Tätigkeitsbeschrieb - Sekretariat Betrieb Infopoint_v1.0.pdf" target="_blank">Betrieb Infopoint</a></li>
-      <li><a href="/helfer/Tätigkeitsbeschrieb - Sekretariat Ticketkontrolle_v1.0.pdf" target="_blank">Ticketkontrolle</a></li>
+      <li><a href="/helfer/Tätigkeitsbeschrieb%20-%20Sekretariat%20Betrieb%20Infopoint_v1.0.pdf" target="_blank">Betrieb Infopoint</a></li>
+      <li><a href="/helfer/Tätigkeitsbeschrieb%20-%20Sekretariat%20Ticketkontrolle_v1.0.pdf" target="_blank">Ticketkontrolle</a></li>
     </ul>
     <hr class="my-10" />
     <h3>Bereich Personal</h3>
     <ul>
-      <li><a href="/helfer/Tätigkeitsbeschrieb - Personal Helferbüro_v1.0.pdf" target="_blank">Mitarbeit Helferbüro</a></li>
+      <li><a href="/helfer/Tätigkeitsbeschrieb%20-%20Personal%20Helferbüro_v1.0.pdf" target="_blank">Mitarbeit Helferbüro</a></li>
     </ul>
   </div>
 </template>
