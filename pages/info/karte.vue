04e5fc2eb4a60735ea0320d32e4b2bd20b155c7b
Stefan Jucker <stefan.jucker@gmail.com>
2024-06-19 20:09:05 +0200

    Update karten

--- pages/info/karte.vue
+++ pages/info/karte.vue
@@ -6,8 +6,8 @@
       Interaktive Karte
     </NuxtLink>
     <div class="flex flex-col xl:flex-row gap-2">
-      <NuxtImg provider="cloudflare" :src="cloudflareUrl('7d8a63cf-334d-4271-2380-44e98e765100')" loading="lazy" class="max-w-full w-[600px] rounded" />
-      <NuxtImg provider="cloudflare" :src="cloudflareUrl('07edda40-48f3-45bb-c4ed-0cbb8ebe5c00')" loading="lazy" class="max-w-full w-[600px] rounded" />
+      <NuxtImg provider="cloudflare" :src="cloudflareUrl('4e560ebf-56f7-4617-4974-b5a0defe6d00')" loading="lazy" class="max-w-full w-[600px] rounded" />
+      <NuxtImg provider="cloudflare" :src="cloudflareUrl('7eb27f61-5b6c-4f11-2c77-8dd0e4e7c400')" loading="lazy" class="max-w-full w-[600px] rounded" />
     </div>
     Als PDF <a href="/info/karte.pdf" target="_blank">herunterladen</a>
   </div>
