6c9de561fb38f4ed63643c56feb76fd7eddd6a25
Stefan Jucker <stefan.jucker@gmail.com>
2024-06-19 11:48:36 +0200

    Prepare screens-page

---
+++ pages/screens.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Screens kaufen',
+})
+</script>
+
+<template>
+  <div class="prose prose-a:text-blau max-w-3xl">
+    <h2 class="text-rot">Screens kaufen</h2>
+    <p>Nach dem grossen Fest können alle Screens gekauft werden. Weitere Infos folgen...</p>
+  </div>
+</template>
