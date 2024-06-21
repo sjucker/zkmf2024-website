f5bd5eb5bc961e95507a225fb8de79dc5b5b7461
Stefan Jucker <stefan.jucker@gmail.com>
2024-06-21 17:04:48 +0200

    Screens

--- pages/screens.vue
+++ pages/screens.vue
@@ -7,6 +7,14 @@ useSeoMeta({
 <template>
   <div class="prose prose-a:text-blau max-w-3xl">
     <h2 class="text-rot">Screens kaufen</h2>
-    <p>Nach dem grossen Fest können alle Screens gekauft werden. Weitere Infos folgen...</p>
+    <p>Nach dem grossen Fest können alle Screens gekauft werden.</p>
+    <h3>Modelle</h3>
+    <ul>
+      <li>2x <a href="/info/98P745.pdf" target="_blank">98P745</a>, netto je <b>CHF 1'899.-</b></li>
+      <li>2x <a href="/info/85P745.pdf" target="_blank">85P745</a>, netto je <b>CHF 1'399.-</b></li>
+      <li>2x <a href="/info/75C645.pdf" target="_blank">75C645</a>, netto je <b>CHF 999.-</b></li>
+      <li>8x <a href="/info/65P635.pdf" target="_blank">65P635</a>, netto je <b>CHF 549.-</b></li>
+    </ul>
+    <p>Interesse? Dann melde dich <a href="mailto:info@zkmf2024.ch">hier</a>!</p>
   </div>
 </template>
