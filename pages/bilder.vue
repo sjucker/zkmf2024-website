e7d13779c857c2583c488cf5c5314ed74a952af2
Stefan Jucker <stefan.jucker@gmail.com>
2024-06-28 21:47:26 +0200

    Typo

--- pages/bilder.vue
+++ pages/bilder.vue
@@ -2,7 +2,7 @@
   <div class="prose prose-a:text-blau">
     <h2 class="text-rot">Bilder</h2>
     <NuxtImg src="/bilder.jpg" class="rounded-2xl"></NuxtImg>
-    <p>Eine grosse Anzahl von <a href="https://photos.app.goo.gl/35f4WwALUPhXLizF7">Bilder finden Sie hier</a>.</p>
+    <p>Eine grosse Anzahl von <a href="https://photos.app.goo.gl/35f4WwALUPhXLizF7">Bildern finden Sie hier</a>.</p>
   </div>
 </template>
 <script setup lang="ts">
