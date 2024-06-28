4a2bab2d1501a693736781fd137c7704cbad680b
Stefan Jucker <jucker@mptechnology.ch>
2024-06-28 08:36:48 +0200

    Screens

--- pages/screens.vue
+++ pages/screens.vue
@@ -6,15 +6,25 @@ useSeoMeta({
 
 <template>
   <div class="prose prose-a:text-blau max-w-3xl">
-    <h2 class="text-rot">Screens kaufen</h2>
-    <p>Nach dem grossen Fest können alle Screens gekauft werden.</p>
-    <h3>Modelle</h3>
+    <h2 class="text-rot">Screens zu verkaufen!</h2>
+    <p>
+      Die am ZKMF2024 in den Wettspiellokalen und in den Festzelten verwendeten, neuwertigen Screens (digitale Anzeigetafeln, Fernseher) werden zu einem
+      deutlich reduzierten Preis in der Originalverpackung abgegeben.<br />
+      Zur Verfügung stehen:
+    </p>
     <ul>
-      <li>2x <a href="/info/98P745.pdf" target="_blank">98P745</a>, netto je <b>CHF 1'899.-</b></li>
-      <li>2x <a href="/info/85P745.pdf" target="_blank">85P745</a>, netto je <b>CHF 1'399.-</b></li>
-      <li>2x <a href="/info/75C645.pdf" target="_blank">75C645</a>, netto je <b>CHF 999.-</b></li>
-      <li>8x <a href="/info/65P635.pdf" target="_blank">65P635</a>, netto je <b>CHF 549.-</b></li>
+      <li>2x 98“ <a href="/info/98P745.pdf" target="_blank">TCL 98P745</a> Nettopreis exkl. 1'691.85</li>
+      <li>2x 85“ <a href="/info/85P745.pdf" target="_blank">TCL P745</a> Nettopreis exkl. 1'263.65</li>
+      <li>2x 75“ <a href="/info/75C645.pdf" target="_blank">TCL C645</a> Nettopreis exkl. 855.15</li>
+      <li>8x 65“ <a href="/info/65P635.pdf" target="_blank">TCL P635</a> Nettopreis exkl. 479.85</li>
     </ul>
-    <p>Interesse? Dann melde dich <a href="mailto:info@zkmf2024.ch">hier</a></p>
+    <p>
+      Die Screens können nach Absprache bei der SmartEfficient AG, Birmensdorferstrasse 21, 8142 Uitikon Waldegg abgeholt werden.<br />
+      Die Anfragen werden in der Reihenfolge ihres Eingangs berücksichtigt – «es hät solangs hät».
+    </p>
+    <p>
+      Bist du an diesem attraktiven Angebot interessiert? Dann sende eine E-Mail an <a href="mailto:info@zkmf2024.ch">info@zkmf2024.ch</a>.<br />
+      Fragen beantwortet Rolf Spitzli, Tel.: 079 126 80 00.
+    </p>
   </div>
 </template>
