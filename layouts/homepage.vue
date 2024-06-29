ce3a08f1695b64d51320c4aa5b6db6877d532f73
Stefan Jucker <stefan.jucker@gmail.com>
2024-06-29 09:44:14 +0200

    Remove helper-ribbon

--- layouts/homepage.vue
+++ layouts/homepage.vue
@@ -1,10 +1,5 @@
 <template>
   <Consent />
-  <div class="absolute top-0 left-0 overflow-hidden w-[250px] h-[250px] z-10" :class="mobileMenuVisible ? 'hidden' : 'visible'">
-    <div class="absolute transform -rotate-45 bg-rot text-center text-white text-2xl font-semibold py-2 -left-[180px] top-[30px] w-[500px]">
-      <NuxtLink to="/helfen">HelferInnen<br />gesucht!</NuxtLink>
-    </div>
-  </div>
 
   <div class="min-h-dvh min-w-full font-body">
     <div class="w-full h-dvh bg-blau" :class="mobileMenuVisible ? 'visible' : 'hidden'">
