5156278fe1a24d3c377bb60398b854229d02ccac
Stefan Jucker <stefan.jucker@gmail.com>
2024-08-05 21:32:52 +0200

    Videos landing page

---
+++ pages/videos.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="prose prose-a:text-blau">
+    <h2 class="text-rot">Videos bald verfügbar...</h2>
+  </div>
+</template>
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Videos',
+})
+</script>
