9ec8f23b3584cf54e870f32a5dc3f33a2aaa4387
Stefan Jucker <jucker@mptechnology.ch>
2024-09-04 12:25:04 +0200

    Videos title

--- pages/videos.vue
+++ pages/videos.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="prose prose-a:text-blau">
-    <h2 class="text-rot">Freitag</h2>
+    <h2 class="text-rot">Videos</h2>
+    <h3 class="text-blau">Freitag</h3>
     <div class="aspect-w-16 aspect-h-9">
       <iframe src="https://www.youtube.com/embed/MWGMAOJFWok"></iframe>
     </div>
     <hr />
-    <h2 class="text-rot">Samstag</h2>
+    <h3 class="text-blau">Samstag</h3>
     <div class="aspect-w-16 aspect-h-9">
       <iframe src="https://www.youtube.com/embed/v1LAyJgnD60"></iframe>
     </div>
     <hr />
-    <h2 class="text-rot">Sonntag</h2>
+    <h3 class="text-blau">Sonntag</h3>
     <div class="aspect-w-16 aspect-h-9">
       <iframe src="https://www.youtube.com/embed/04EDcqyt1zY"></iframe>
     </div>
