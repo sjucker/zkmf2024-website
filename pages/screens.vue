5b0101295947fadedd939ef819c6d407e85a5129
Stefan Jucker <jucker@mptechnology.ch>
2024-10-18 10:33:43 +0200

    Screens sold

--- pages/screens.vue
+++ pages/screens.vue
@@ -6,23 +6,6 @@ useSeoMeta({
 
 <template>
   <div class="prose prose-a:text-blau max-w-3xl">
-    <h2 class="text-rot">Screens zu verkaufen!</h2>
-    <p>
-      Die am ZKMF2024 in den Wettspiellokalen und in den Festzelten verwendeten, neuwertigen Screens (digitale Anzeigetafeln, Fernseher) werden zu einem
-      deutlich reduzierten Preis in der Originalverpackung abgegeben.<br />
-      Zur Verfügung stehen:
-    </p>
-    <ul>
-      <li>1x 85“ <a href="/info/85P745.pdf" target="_blank">TCL 85P745</a> Nettopreis <b>exkl. 1'399.-</b></li>
-      <li>1x 65“ <a href="/info/65P635.pdf" target="_blank">TCL 65P635</a> Nettopreis <b>exkl. 529.-</b></li>
-    </ul>
-    <p>
-      Die Screens können nach Absprache bei der SmartEfficient AG, Birmensdorferstrasse 21, 8142 Uitikon Waldegg abgeholt werden.<br />
-      Die Anfragen werden in der Reihenfolge ihres Eingangs berücksichtigt – «es hät solangs hät».
-    </p>
-    <p>
-      Bist du an diesem attraktiven Angebot interessiert? Dann sende eine E-Mail an <a href="mailto:info@zkmf2024.ch">info@zkmf2024.ch</a>.<br />
-      Fragen beantwortet Rolf Spitzli, Tel.: 079 126 80 00.
-    </p>
+    <h2 class="text-rot">Alle Screens sind verkauft...</h2>
   </div>
 </template>
