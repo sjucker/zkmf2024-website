afe2c796c4116c6d17a959e1b0678a6177b30358
Stefan Jucker <stefan.jucker@gmail.com>
2025-04-03 22:32:25 +0200

    Add judge-images directly instead from Cloudflare

--- components/JudgeImage.vue
+++ components/JudgeImage.vue
@@ -1,14 +1,6 @@
 <template>
   <div class="flex flex-col items-center flex-wrap max-w-full sm:w-[300px] lg:w-[350px] p-3 md:p-6">
-    <NuxtImg
-      provider="cloudflare"
-      :src="cloudflareUrl(src)"
-      loading="lazy"
-      :alt="name"
-      :title="name"
-      class="rounded-full shadow shadow-gray-500"
-      densities="x1"
-    />
+    <NuxtImg :src="'/judge/' + src + '.jpg'" loading="lazy" :alt="name" :title="name" class="rounded-full shadow shadow-gray-500" densities="x1" />
     <div class="font-bold text-center">{{ name }}</div>
     <div class="text-sm text-center whitespace-pre">{{ modul }}</div>
   </div>
